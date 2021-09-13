a2b888cf47ba064818cf9916fde7a850d74ec20a
Nitish Kumar <nkysy713@gmail.com>
2021-09-14 02:07:20 +0530

    Create Register and Login Controller

---
+++ BookReadingEvent1/Controllers/RegisterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookReadingEvent1.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
