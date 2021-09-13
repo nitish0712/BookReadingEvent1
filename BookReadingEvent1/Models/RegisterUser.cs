2669eb48d709f30f984d1521dbd863412b6979a3
Nitish Kumar <nkysy713@gmail.com>
2021-09-14 02:25:36 +0530

    Register User Model creation

---
+++ BookReadingEvent1/Models/RegisterUser.cs
@@ -0,0 +1,21 @@
+﻿using BookReadingEvent1.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookReadingEvent1.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        [UserNameValidation]
+        public string UserName { get; set; }
+        
+        [Required]
+        [EmailValidation]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+
+    }
+}
