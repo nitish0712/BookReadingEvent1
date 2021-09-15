4318850539d5317e0ad772f2b7b104244673cb1d
Nitish Kumar <nkysy713@gmail.com>
2021-09-16 05:04:49 +0530

    Username validation

--- BookReadingEvent1/Validations/UserNameValidation.cs
+++ BookReadingEvent1/Validations/UserNameValidation.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookReadingEvent1.Basic1;
+using BookReadingEvent1.Common;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,5 +10,27 @@ namespace BookReadingEvent1.Validations
 {
     public class UserNameValidation : ValidationAttribute
     {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            IEnumerable<User> users = new AllUsers1().GetUsers;
+            var property1 = validationContext.ObjectType.GetProperty("UserName");
+            var property2 = validationContext.ObjectType.GetProperty("EmailId");
+            string userName = (string)property1.GetValue(validationContext.ObjectInstance);
+            string emailId = (string)property2.GetValue(validationContext.ObjectInstance);
+            var queryUserName = (from u in users
+                                 where u.UserName.Equals(userName, System.StringComparison.OrdinalIgnoreCase)
+                                 select u).ToList().Count;
+
+
+
+            if (queryUserName != 0)
+            {
+                return new ValidationResult("" + validationContext.DisplayName + "already exists");
+            }
+            else
+                return ValidationResult.Success;
+
+
+        }
     }
 }
