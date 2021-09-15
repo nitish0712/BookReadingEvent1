ff5b03f6848a5b2d6e5eae9a1056622df4091fc9
Nitish Kumar <nkysy713@gmail.com>
2021-09-16 05:16:25 +0530

    About Page completed

--- BookReadingEvent1/Basic2/EventInitializer.cs
+++ BookReadingEvent1/Basic2/EventInitializer.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace BookReadingEvent1.Basic2
 {
-    public class EventInitializer : DropCreateDatabaseIfModelChanges<BookReadingContext>
+    public class EventInitializer : DropCreateDatabaseAlways<BookReadingContext>
     {
         protected override void Seed(BookReadingContext context)
         {
@@ -20,14 +20,14 @@ namespace BookReadingEvent1.Basic2
             context.SaveChanges();
             var events = new List<Event>
                 {
-               new Event{ Title="Basics of Competetive Programming",Date=DateTime.Now, Location="Plot 13", Duration=3, StartTime=DateTime.Now, Description=" A GOOD BOOK 1",OtherDetails=null,Type=EventType.PRIVATE,InviteByEmail=null,UserId="Kavan Patel"  }
+               new Event{ Title="Basics of Competetive Programming",Date=DateTime.Now, Location="Gurugram", Duration=4, StartTime=DateTime.Now, Description=" GUIDE TO COMPETETIVE PROGRAMMING",OtherDetails=null,Type=EventType.PRIVATE,InviteByEmail=null,UserId="Nitish Kumar"  }
                };
             events.ForEach(s => context.Events.Add(s));
             context.SaveChanges();
             var roles = new List<Role>
                 {
-                new Role{AssignedRole="Admin",UserId="Mayabk Gupta"},
-                new Role {AssignedRole = "User", UserId = "Nitish Kumar" },
+                new Role{AssignedRole="User",UserId="Mayank Gupta"},
+                new Role{AssignedRole = "Admin", UserId = "Nitish Kumar" },
                 new Role{AssignedRole="User",UserId="Praveen"},
             };
             roles.ForEach(s => context.Roles.Add(s));
