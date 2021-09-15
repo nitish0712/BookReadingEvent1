d303d40aa8745396b2fc1763bc8a0e3fe2f10c3a
Nitish Kumar <nkysy713@gmail.com>
2021-09-16 04:58:15 +0530

    Email validation

---
+++ BookReadingEvent1/Basic2/AllUsers2.cs
@@ -0,0 +1,24 @@
+﻿using BookReadingEvent1.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookReadingEvent1.Basic2
+{
+    public class AllUsers2
+    {
+        public IEnumerable<User> GetUsers
+        {
+            get
+            {
+                using (BookReadingContext context = new BookReadingContext())
+                {
+                    IEnumerable<User> users = (from u in context.Users
+                                               select u).ToList();
+                    return users;
+                }
+            }
+        }
+    }
+}
