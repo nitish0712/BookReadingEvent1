17c9e4abc1ef41aacd6e29a1d4f249b938f79e82
Nitish Kumar <nkysy713@gmail.com>
2021-09-16 04:15:12 +0530

    Events list created and shown to the user

--- BookReadingEvent1/Controllers/HomeController.cs
+++ BookReadingEvent1/Controllers/HomeController.cs
@@ -11,10 +11,10 @@ namespace BookReadingEvent1.Controllers
 {
     public class HomeController : Controller
     {
-        //public ActionResult Index()
-        //{
-            //return View();
-        //}
+        public ActionResult Index()
+        {
+            return View();
+        }
 
         public ActionResult About()
         {
@@ -22,8 +22,29 @@ namespace BookReadingEvent1.Controllers
 
             IEnumerable<Event> upcomingEvents;
             IEnumerable<Event> events = new AllEvents1().GetEvents;
+            IEnumerable<Event> missedEvents;
 
-            return View();
+            if (User.Identity.IsAuthenticated)
+            {
+                upcomingEvents = events.Where(e => (e.Date > DateTime.Now.Date) || ((e.Date == DateTime.Now.Date) && (e.Date.TimeOfDay > DateTime.Now.TimeOfDay))).ToList();
+                upcomingEvents = upcomingEvents.Where(e => (e.Type == EventType.PRIVATE && e.UserId == User.Identity.Name) || (e.Type == EventType.PUBLIC)).ToList();
+                missedEvents = events.Where(e => (e.Date < DateTime.Now.Date) || ((e.Date == DateTime.Now.Date) && (e.Date.TimeOfDay < DateTime.Now.TimeOfDay))).ToList();
+                missedEvents = missedEvents.Where(e => (e.Type == EventType.PRIVATE && e.UserId == User.Identity.Name) || (e.Type == EventType.PUBLIC)).ToList();
+            }
+            else
+            {
+                events = events.Where(x => x.Type == EventType.PUBLIC);
+                upcomingEvents = events.Where(e => (e.Date > DateTime.Now.Date) || ((e.Date == DateTime.Now.Date) && (e.Date.TimeOfDay > DateTime.Now.TimeOfDay))).ToList();
+                upcomingEvents = upcomingEvents.Where(e => e.Type == EventType.PUBLIC).ToList();
+                missedEvents = events.Where(e => (e.Date < DateTime.Now.Date) || ((e.Date == DateTime.Now.Date) && (e.Date.TimeOfDay < DateTime.Now.TimeOfDay))).ToList();
+                missedEvents = missedEvents.Where(e => e.Type == EventType.PUBLIC).ToList();
+            }
+
+            List<IEnumerable<EventModel>> eventModels = new List<IEnumerable<EventModel>>();
+            eventModels.Add(new EventToEventModelHelper().GetEventModels(missedEvents));
+            eventModels.Add(new EventToEventModelHelper().GetEventModels(upcomingEvents));
+
+            return View(eventModels);
         }
 
         //public ActionResult Contact()
