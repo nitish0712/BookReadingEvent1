c15eafc3fe437f62fddb973c343d9bc3283f958b
Nitish Kumar <nkysy713@gmail.com>
2021-09-20 05:09:30 +0530

    Action result my events

--- BookReadingEvent1/Controllers/BookReadingEventsController.cs
+++ BookReadingEvent1/Controllers/BookReadingEventsController.cs
@@ -1,4 +1,6 @@
-﻿using BookReadingEvent1.Common;
+﻿using BookReadingEvent1.Basic1;
+using BookReadingEvent1.Common;
+using BookReadingEvent1.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,9 +28,17 @@ namespace BookReadingEvent1.Controllers
         public ActionResult AllEvents()
         {
             AllEvents1 allEvents = new AllEvents1();
-            IEnumerable<Event> events = allEvents.getEvents;
+            IEnumerable<Event> events = allEvents.GetEvents;
 
-            return View(new EventToEventModelHeler().GetEventModels(events));
+            return View(new EventToEventModelHelper().GetEventModels(events));
+        }
+
+        public ActionResult MyEvents()
+        {
+            string UserName = getUserName;
+            MyEvents1 getMyEvents = new MyEvents1();
+            var myEvents = getMyEvents.GetMyEvents(UserName);
+            return View(new EventToEventModelHelper().GetEventModels(myEvents));
         }
     }
 }
