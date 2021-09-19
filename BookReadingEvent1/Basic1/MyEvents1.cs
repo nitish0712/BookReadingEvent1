e300776e40dd23754d14a09a283a6c04c7c348a0
Nitish Kumar <nkysy713@gmail.com>
2021-09-20 05:14:51 +0530

    My Events completed

--- BookReadingEvent1/Basic1/MyEvents1.cs
+++ BookReadingEvent1/Basic1/MyEvents1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookReadingEvent1.Basic2;
+using BookReadingEvent1.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +9,11 @@ namespace BookReadingEvent1.Basic1
 {
     public class MyEvents1
     {
-
+        public IEnumerable<Event> GetMyEvents(string UserName)
+        {
+            MyEvents2 getMyEvents = new MyEvents2();
+            IEnumerable<Event> myEvents = getMyEvents.GetMyEvents(UserName);
+            return myEvents;
+        }
     }
 }
---
+++ BookReadingEvent1/Basic2/MyEvents2.cs
@@ -0,0 +1,28 @@
+﻿using BookReadingEvent1.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookReadingEvent1.Basic2
+{
+    public class MyEvents2
+    {
+        public IEnumerable<Event> GetMyEvents(string userName)
+        {
+
+
+            using (BookReadingContext db = new BookReadingContext())
+            {
+
+
+                var evt = (from e in db.Events
+                           where e.UserId == userName
+                           select e).ToList();
+
+                return evt;
+            }
+
+        }
+    }
+}
