60fd3b98a9612e1d45521830f60aa12f03c83514
Nitish Kumar <nkysy713@gmail.com>
2021-09-21 19:58:05 +0530

    Role provider class

---
+++ BookReadingEvent1/Basic1/UserRole1.cs
@@ -0,0 +1,16 @@
+﻿using BookReadingEvent1.Basic2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookReadingEvent1.Basic1
+{
+    public class UserRole1
+    {
+        public string[] GetRoles(string userName)
+        {
+            return new UserRole2().GetRoles(userName);
+        }
+    }
+}
